dce48447a80171d70bc6fb96ca8619434267e2e7
lkrisztian99 <90862618+lkrisztian99@users.noreply.github.com>
2021-11-10 14:17:50 +0100

    Update error.cpp

--- error.cpp
+++ error.cpp
@@ -15,16 +15,16 @@ int main(){
  	//string s = "ape"; bool c = "fool" > s; if (c) cout << "9.Success!\n";
 	//string s = "ape"; if (s != "fool") cout << "10.Success!\n";
 	//string s = "ape"; if (s != "fool") cout << "11.Success!\n";
-	//string s = "ape"; string check = "nothing"; if (check != s + "fool") { cout << "Success!\n"; }
-	//vector<char> v(5); for (int i = 0; i < v.size(); ++i); cout << "Success!\n";
-	//vector<char> v(5); for (int i = 0; i <= v.size(); ++i); cout << "Success!\n";
-	//string s = "Success!\n"; for (int i = 0; i < s.size(); ++i) cout << s[i];
-	//if (true) cout << "Success!\n"; else cout << "Fail!\n";
-	//int x = 2000; int c = x; if (c == 2000) cout << "Success!\n";
-	//string s = "Success!\n"; for (int i = 0; i < 10; i++) cout << s[i];
-	vector<int> v(5); for (int i = 0; i <= v.size(); ++i); cout << "Success!\n";
-	//int i = 0; int j = 9; while (j < 10) j++; if (j > i) cout << "Success!\n";
-	//double x = 2; double d = 5 / (x – 1); if (d == 2 * (x + 0.5)) cout << "Success!\n";
+	//string s = "ape"; string check = "nothing"; if (check != s + "fool") { cout << "Success!\n"; } //12
+	//vector<char> v(5); for (int i = 0; i < v.size(); ++i)/*;*/ cout << "Success!\n"; //13
+	//vector<char> v(5); for (int i = 0; i <= v.size(); ++i)/*;*/ cout << "Success!\n"; //14
+	//string s = "Success!"; for (int i = 0; i < s.size(); ++i) cout << s[i]; //15
+	//if (true) cout << "Success!\n"; else cout << "Fail!\n"; //16
+	//int x = 2000; int c = x; if (c == 2000) cout << "Success!\n"; //17
+	//string s = "Success!\n"; for (int i = 0; i < 7; i++) cout << s[i]; //18
+	vector<int> v(5); for (int i = 0; i <= v.size(); ++i); cout << "Success!\n"; //19
+	//int i = 0; int j = 9; while (i < 10) {i++; if (j > i) cout << "Success!\n";} //20
+	//double x = 2; double d = 5 / (x – 1); if (d == 2 * (x + 0.5)) cout << "Success!\n"; //21
         //vector<string> s; s.push_back("Success!\n"); for (int i = 0; i < s.size(); ++i) cout << s[i];
 	//int i = 0; int j = 11; while (i < 10) ++i; if (j > i) cout << "Success!\n";
 	//double x = 2; double d = 5 / (x – 1); if (d = 2 * (x + 0.5)) cout << "Success!\n";
